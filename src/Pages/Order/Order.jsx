5a44922fc6d6981a4e45f000e06b902c30c065f9
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-29 14:43:52 +0600

    react tabs

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,5 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Cover from "../../components/Cover";
+import coverImg from "../../assets/shop/banner2.jpg";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../Hooks/useMenu";
 export default function Order() {
-  return <div className="max-w-6xl mx-auto"></div>;
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menus] = useMenu();
+  const deserts = menus.filter((menu) => menu.category === "dessert");
+  const pizzas = menus.filter((menu) => menu.category === "pizza");
+  const salads = menus.filter((menu) => menu.category === "salad");
+  const soup = menus.filter((menu) => menu.category === "soup");
+  const offered = menus.filter((menu) => menu.category === "offered");
+  return (
+    <div className="max-w-6xl mx-auto">
+      <Cover title={"order"} image={coverImg} />
+      <div className="my-6 flex items-center justify-center gap-6">
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            <Tab>Salad</Tab>
+            <Tab>Soup</Tab>
+            <Tab>Pizza </Tab>
+            <Tab>Drinks </Tab>
+            <Tab>Desserts</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
